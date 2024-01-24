7e2c27e5f5167251d512a84f8666d07353307fde
Pablo Simon <pablosimonsr@gmail.com>
2024-01-24 21:21:41 +0100

    Completed ingestion

--- dags/etl_local_to_posgres/etl_local_to_posgres.py
+++ dags/etl_local_to_posgres/etl_local_to_posgres.py
@@ -1,10 +1,12 @@
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
-from datetime import datetime
-
-import pandas as pd
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import Variable
+
+import pandas as pd
+
+from datetime import datetime
+
 from sqlalchemy import create_engine
 
 
@@ -12,76 +14,73 @@ import os
 
 DAG_ID = 'etl_local_to_posgres'
 POSTGRE_CONN_ID = 'postgres_conn'
-DATASET_NAME = 'churn_prediction_dataset'
-DATASET_PATH = f"/opt/airflow/data/raw/{DATASET_NAME}.csv"
+TABLE_NAME = 'churn_prediction_dataset'
+DATASET_PATH = f"/opt/airflow/data/raw/churn_prediction_dataset.csv"
 
 PK='Customer_ID'
 
-def get_posgres_cols(table_name:str) -> list:
+def bulk_csv_to_postgres() -> list:
     """
-    Get the columns and their types from a postgres table.
-    :param table_name: str, Name of the PostgreSQL table
-    :return: list, List of column names in the table
-    """
-    try:
-        with PostgresHook(postgres_conn_id=POSTGRE_CONN_ID).get_conn() as conn:
-            query = f"""
-                SELECT column_name
-                FROM information_schema.columns
-                WHERE table_name = '{table_name}'
-                ORDER BY ordinal_position;
-            """
-            df = pd.read_sql(query, conn)
-            return df['column_name'].tolist()
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return []
+    Copy from csv to postgres table.
     
-
-def upsert_data_to_postgres():
-    print(f'loading data from {DATASET_PATH} to postgresql...')
+    :return: list of table columns inserted
+    :rtype: list
+    """
+    # Create connection to postgres
+    pg_hook = PostgresHook(postgres_conn_id=POSTGRE_CONN_ID)
+    engine = create_engine(pg_hook.get_uri())
+    
+    # Load data from csv to pandas dataframe
     df = pd.read_csv(DATASET_PATH, sep=';', decimal=',')
-    print(f'loaded {df.shape[0]} rows')
-    postgres_cols:list = get_posgres_cols(DATASET_NAME)
     
-    print(f"postgres columns: {postgres_cols}")
+    # lowercase column names
+    df.columns = df.columns.str.lower()
     
-    # create a connection with postgres
-    postgres_hook = PostgresHook(postgres_conn_id=POSTGRE_CONN_ID)
-    with postgres_hook.get_conn() as connection, connection.cursor() as cursor:
-        cols = df.columns.tolist()
-        cols.pop(cols.index(PK))
-        update_set = ', '.join([f'{col}=excluded.{col}' for col in cols])
+    # Upsert data to postgres
+    df.to_sql(f"{TABLE_NAME}_temp", engine, index=False, if_exists='replace')
+    
+    print(f"Upserted data to {TABLE_NAME}_temp table")
+    
+    return df.columns.tolist()
+
+def upsert_data_to_postgres(cols: list) -> None:
+    """
+    Upsert data from postgres table to another postgres table.
+    """
+    # Create connection to postgres
+    pg_hook = PostgresHook(postgres_conn_id=POSTGRE_CONN_ID)
+    engine = create_engine(pg_hook.get_uri())
+    
+    # Upsert data to postgres
+    with engine.connect() as conn:
+        conn.execute(f"""
+            INSERT INTO {TABLE_NAME} ({', '.join(cols)})
+            SELECT {', '.join(cols)}
+            FROM {TABLE_NAME}_temp
+            ON CONFLICT ({PK}) DO UPDATE
+            SET {', '.join([f"{col}=EXCLUDED.{col}" for col in cols])}
+        """)
         
-        # df cols lower case
-        df.columns = df.columns.str.lower()
-        df = df[postgres_cols]
+        conn.execute(f"DROP TABLE {TABLE_NAME}_temp")
         
-        # upsert the whole dataframe to a new table, the PK=customer_id
-        added_rows = 0
-        query = f"INSERT INTO {DATASET_NAME} ({', '.join(df.columns)})"
-        query += "\n VALUES "
-        
-        values = ["("+ ', '.join([f"'{row[col]}'" if isinstance(row[col], str) else str(row[col]) for col in df.columns]) + ")" for _, row in df.iterrows()]
-        
-        query += ",\n".join(values)
-        query += f"\n ON CONFLICT ({PK}) DO UPDATE SET {update_set};"
-        
-        print(f'executed query: {query}')
-        
-        cursor.execute(query)
-        
-        connection.commit()
-        print(f'upserted {added_rows} rows')
+    print(f"Upserted data to {TABLE_NAME} table")
+    
+    
     
 with DAG(
     dag_id=DAG_ID,
     start_date=datetime.now()
     ):
     
-    load_data_to_postgres = PythonOperator(
-        task_id='load_data_to_postgres',
-        python_callable=upsert_data_to_postgres
+    t1 = PythonOperator(
+        task_id='bulk_csv_to_postgres',
+        python_callable=bulk_csv_to_postgres
     )
     
-    load_data_to_postgres
+    t2 = PythonOperator(
+        task_id='upsert_data_to_postgres',
+        python_callable=upsert_data_to_postgres,
+        op_kwargs={'cols': t1.output}
+    )
+    
+    t1 >> t2
