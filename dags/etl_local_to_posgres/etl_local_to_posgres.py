46b1d14d445b1a05a73949c81d56d78748d2a41a
Pablo Simon <pablosimonsr@gmail.com>
2024-01-24 22:16:41 +0100

    Refactored

--- dags/etl_local_to_posgres/etl_local_to_posgres.py
+++ dags/etl_local_to_posgres/etl_local_to_posgres.py
@@ -1,16 +1,17 @@
-from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
-from airflow.hooks.postgres_hook import PostgresHook
-from airflow.models import Variable
-
 import pandas as pd
 
 from datetime import datetime
 
-from sqlalchemy import create_engine
-
-
 import os
+
+import sys
+
+
+from etl_local_to_posgres.ingestor.ingestor_csv_to_postgres import IngestorCSVToPostgres
+from etl_local_to_posgres.ingestor.i_ingestor import IIngestor
+
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
 
 DAG_ID = 'etl_local_to_posgres'
 POSTGRE_CONN_ID = 'postgres_conn'
@@ -19,54 +20,14 @@ DATASET_PATH = f"/opt/airflow/data/raw/churn_prediction_dataset.csv"
 
 PK='Customer_ID'
 
-def bulk_csv_to_postgres() -> list:
-    """
-    Copy from csv to postgres table.
     
-    :return: list of table columns inserted
-    :rtype: list
-    """
-    # Create connection to postgres
-    pg_hook = PostgresHook(postgres_conn_id=POSTGRE_CONN_ID)
-    engine = create_engine(pg_hook.get_uri())
-    
-    # Load data from csv to pandas dataframe
-    df = pd.read_csv(DATASET_PATH, sep=';', decimal=',')
-    
-    # lowercase column names
-    df.columns = df.columns.str.lower()
-    
-    # Upsert data to postgres
-    df.to_sql(f"{TABLE_NAME}_temp", engine, index=False, if_exists='replace')
-    
-    print(f"Upserted data to {TABLE_NAME}_temp table")
-    
-    return df.columns.tolist()
+etl_process:IIngestor = IngestorCSVToPostgres(
+    postgres_conn_id='postgres_conn',
+    table_name='churn_prediction_dataset',
+    dataset_path="/opt/airflow/data/raw/churn_prediction_dataset.csv",
+    pk='Customer_ID'
+)
 
-def upsert_data_to_postgres(cols: list) -> None:
-    """
-    Upsert data from postgres table to another postgres table.
-    """
-    # Create connection to postgres
-    pg_hook = PostgresHook(postgres_conn_id=POSTGRE_CONN_ID)
-    engine = create_engine(pg_hook.get_uri())
-    
-    # Upsert data to postgres
-    with engine.connect() as conn:
-        conn.execute(f"""
-            INSERT INTO {TABLE_NAME} ({', '.join(cols)})
-            SELECT {', '.join(cols)}
-            FROM {TABLE_NAME}_temp
-            ON CONFLICT ({PK}) DO UPDATE
-            SET {', '.join([f"{col}=EXCLUDED.{col}" for col in cols])}
-        """)
-        
-        conn.execute(f"DROP TABLE {TABLE_NAME}_temp")
-        
-    print(f"Upserted data to {TABLE_NAME} table")
-    
-    
-    
 with DAG(
     dag_id=DAG_ID,
     start_date=datetime.now()
@@ -74,12 +35,12 @@ with DAG(
     
     t1 = PythonOperator(
         task_id='bulk_csv_to_postgres',
-        python_callable=bulk_csv_to_postgres
+        python_callable=etl_process.bulk_csv_to_postgres
     )
     
     t2 = PythonOperator(
         task_id='upsert_data_to_postgres',
-        python_callable=upsert_data_to_postgres,
+        python_callable=etl_process.upsert_data_to_postgres,
         op_kwargs={'cols': t1.output}
     )
     
